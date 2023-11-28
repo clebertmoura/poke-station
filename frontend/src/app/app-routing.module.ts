d2633b76af5dff2474284e2dc498048f21508d1d
Cleber Moura <cleber.t.moura@gmail.com>
2023-11-27 22:07:36 -0300

    config auth guard

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,14 +2,25 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { TrainersComponent } from './catalog/ui/trainers/trainers.component';
 import { TrainerDetailComponent } from './catalog/ui/trainer-detail/trainer-detail.component';
+import { AuthGuard } from './auth/guards/auth.guard';
 
 const routes: Routes = [
-  { path: '', component: TrainersComponent },
-  { path: 'trainer/:id', component: TrainerDetailComponent },
+  {
+    path: '',
+    component: TrainersComponent,
+    data: { roles: ['ADMIN'] },
+    canActivate: [AuthGuard],
+  },
+  {
+    path: 'trainer/:id',
+    component: TrainerDetailComponent,
+    data: { roles: ['ADMIN'] },
+    canActivate: [AuthGuard],
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
