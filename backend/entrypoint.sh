0e3453c8a9a00830512ba1ed900267d737b763d3
Cleber Moura <cleber.t.moura@gmail.com>
2023-11-28 16:58:03 -0300

    ajuste para acessar a aplicação com localhost

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -5,6 +5,11 @@ HOST_IP=$(ip route | awk '/default/ { print $3 }')
 
 # Add an entry to /etc/hosts with the host's IP
 echo "$HOST_IP app.pokestation.com" >> /etc/hosts
+echo "$HOST_IP auth.pokestation.com" >> /etc/hosts
+sed "1s/127.0.0.1/$HOST_IP/" /etc/hosts >> ./temp_etc_hosts
+echo "" > /etc/hosts
+cat ./temp_etc_hosts >> /etc/hosts
+rm ./temp_etc_hosts
 
 # Start application
 exec "$@"
--- frontend/src/environments/environment.development.ts
+++ frontend/src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  keycloakUrl: 'http://localhost:9090',
-  backendUrl: 'http://localhost:8080/backend',
+  keycloakUrl: 'http://localhost',
+  backendUrl: 'http://localhost/backend',
   pokeApiUrl: 'https://pokeapi.co/api/v2',
 };
